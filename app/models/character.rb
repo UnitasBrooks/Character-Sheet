8d4bbe2eca7a5a2b98814beb2b3bcbba69742f25
Joe Yuhas <joe.yuhas@gmail.com>
2013-10-09 22:53:32 -0700

    added some css and formatting as well as randomly seeded character attributes

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ActiveRecord::Base
 	has_many :weapons
+
+	after_initialize do
+		if self.new_record?
+			self.strength = 3 + rand(6) + rand(6) + rand(6)
+			self.dexterity = 3 + rand(6) + rand(6) + rand(6)
+			self.constitution = 3 + rand(6) + rand(6) + rand(6)
+			self.wisdom = 3 + rand(6) + rand(6) + rand(6)
+			self.intelligence = 3 + rand(6) + rand(6) + rand(6)
+			self.charisma = 3 + rand(6) + rand(6) + rand(6)
+
+		end
+	end
 end
+
